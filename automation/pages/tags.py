d9fcb031ce15f6fc454c029f920f2d73663e6521
Steven Valladares <stevenvalladaresgiron@gmail.com>
2024-09-16 17:45:37 -0400

    [UPD] add styles in tags page

--- automation/pages/tags.py
+++ automation/pages/tags.py
@@ -1,24 +1,34 @@
 import dash
+import dash_bootstrap_components as dbc
 from automation.pages.components import Components
 from automation.pages.components.tags import TagsComponents
 
 dash.register_page(__name__)
 
-layout = dash.html.Div([
-    Components.modal_error(
-        title="Error",
-        modal_id="modal-centered",
-        button_close_id="close-centered",
-        body_id="modal-body"
-    ),
-    Components.modal_confirm(
-        title="Confirmation",
-        modal_id="modal-update_delete-centered",
-        body_id="modal-update-delete-tag-body",
-        yes_button_id="update-delete-tag-yes",
-        no_button_id="update-delete-tag-no"
-    ),
-    Components.page_title('Tags'),
-    TagsComponents.create_tag_form(),
-    TagsComponents.tags_table()
-])
+layout = dbc.Container(
+    [
+        dbc.Breadcrumb(
+            items=[
+                {"label": "Home", "href": "/"},
+                {"label": "Tags", "active": True},
+            ],
+        ),
+        Components.modal_error(
+            title="Error",
+            modal_id="modal-centered",
+            button_close_id="close-centered",
+            body_id="modal-body"
+        ),
+        Components.modal_confirm(
+            title="Confirmation",
+            modal_id="modal-update_delete-centered",
+            body_id="modal-update-delete-tag-body",
+            yes_button_id="update-delete-tag-yes",
+            no_button_id="update-delete-tag-no"
+        ),
+        TagsComponents.create_tag_form(),
+        TagsComponents.tags_table()
+    ],
+    fluid=False,
+    className="my-3"
+)
