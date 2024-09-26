62a43a423980ebe0f1d5e93ef8cf3c707f168e81
know-ai <dev.know.ai@gmail.com>
2024-09-25 23:09:46 -0400

    [ADD] Init working on logic layer for users

--- automation/users/users.py
+++ automation/users/users.py
@@ -1,10 +1,23 @@
 from automation.singleton import Singleton
+import secrets
 
 
 class User:
     r"""
     Documentation here
     """
-    def __init__(self):
+    def __init__(
+            self, 
+            username:str, 
+            role:str, 
+            email:str, 
+            password:str, 
+            name:str=None, 
+            lastname:str=None, 
+            id:str=None):
 
-        pass
+        self.id = secrets.token_hex(4)
+        if id:
+            self.id = id
+        self.username = username
+        
