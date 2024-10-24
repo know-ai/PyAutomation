a04449020a1cd6cc9ece1d980560aec5d2969cb6
know-ai <dev.know.ai@gmail.com>
2024-10-24 08:10:00 -0400

    [UPD] TIMEZONE env variable by configuration

--- automation/__init__.py
+++ automation/__init__.py
@@ -7,6 +7,14 @@ app = Flask(__name__, instance_relative_config=False)
 
 MANUFACTURER = os.environ.get('MANUFACTURER')
 SEGMENT = os.environ.get('SEGMENT')
+TIMEZONE = os.environ.get('TIMEZONE') or "UTC"
+CERT_FILE = os.path.join(".", "ssl", os.environ.get('CERT_FILE') or "")
+KEY_FILE = os.path.join(".", "ssl", os.environ.get('KEY_FILE') or "")
+if not os.path.isfile(CERT_FILE):
+    CERT_FILE = None
+
+if not os.path.isfile(KEY_FILE):
+    KEY_FILE = None
 
 class CreateApp():
     """Initialize the core application."""
