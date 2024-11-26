723ae26d86f056b31f812729424162a125dd318b
know-ai <dev.know.ai@gmail.com>
2024-11-26 08:09:46 -0400

    [ADD] Create alarm when define a tag with IAD algorithm

--- automation/iad/frozen_data.py
+++ automation/iad/frozen_data.py
@@ -3,6 +3,7 @@ from ..buffer import Buffer
 
 data = dict()
 
+
 def __iad(data:Buffer):
     r"""
     Frozen Data Algorithm
@@ -14,18 +15,27 @@ def iad_frozen_data(func, args, kwargs):
     r"""
     Documentation here
     """
+    
     cvt = args[0]
     tag_id = kwargs["id"]
     value = kwargs["value"]
     tag = cvt.get_tag(id=tag_id)
-    if tag.name not in data:
-        data[tag.name] = Buffer()
-    data[tag.name](value)
-    print(f"[{tag.name}]: {value}")
-    print(f"Data: {data[tag.name]}")
-    # Apply IAD logic
-    if len(data[tag.name]) >= data[tag.name].size:
-        kwargs["value"] = __iad(data[tag.name])
-        return func(*args, **kwargs)
+    if tag.frozen_data_detection:
+        if tag.name not in data:
+            data[tag.name] = Buffer()
+            
+            # Create Alarm
+            from automation import PyAutomation
+            app = PyAutomation()
+            alarm_name = f"alarm.iad.frozen.{tag.name}"
+            if not app.alarm_manager.get_alarm_by_name(name=alarm_name):
+                if app.is_db_connected():
+                    app.create_alarm(name=alarm_name, tag=f"{tag.name}", description="The measurement has been repeated for a long time")
+        
+        data[tag.name](value)
+        # Apply IAD logic
+        if len(data[tag.name]) >= data[tag.name].size:
+            kwargs["value"] = __iad(data[tag.name])
+            return func(*args, **kwargs)
 
     return func(*args, **kwargs)
--- automation/iad/out_of_range.py
+++ automation/iad/out_of_range.py
@@ -18,12 +18,22 @@ def iad_out_of_range(func, args, kwargs):
     tag_id = kwargs["id"]
     value = kwargs["value"]
     tag = cvt.get_tag(id=tag_id)
-    if tag.name not in data:
-        data[tag.name] = Buffer()
-    data[tag.name](value)
-    # Apply IAD logic
-    if len(data[tag.name]) >= data[tag.name].size:
-        kwargs["value"] = __iad(data[tag.name])
-        return func(*args, **kwargs)
+    if tag.out_of_range_detection:
+        if tag.name not in data:
+            data[tag.name] = Buffer()
+            
+            # Create Alarm
+            from automation import PyAutomation
+            app = PyAutomation()
+            alarm_name = f"alarm.iad.oor.{tag.name}"
+            if not app.alarm_manager.get_alarm_by_name(name=alarm_name):
+                if app.is_db_connected():
+                    app.create_alarm(name=alarm_name, tag=f"{tag.name}", description="The measurement is out of range")
+        
+        data[tag.name](value)
+        # Apply IAD logic
+        if len(data[tag.name]) >= data[tag.name].size:
+            kwargs["value"] = __iad(data[tag.name])
+            return func(*args, **kwargs)
 
     return func(*args, **kwargs)
