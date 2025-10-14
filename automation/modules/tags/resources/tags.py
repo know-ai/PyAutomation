89398c0368752d87dcc6b942d930968dbd97d73d
knowai <dev.know.ai@gmail.com>
2025-10-14 15:49:27 -0400

    [UPD] Tags read, without token

--- automation/modules/tags/resources/tags.py
+++ automation/modules/tags/resources/tags.py
@@ -25,8 +25,8 @@ write_value_model = api.model("write_value_model", {
 @ns.route('/')
 class TagsCollection(Resource):
 
-    @api.doc(security='apikey')
-    @Api.token_required(auth=True)
+    # @api.doc(security='apikey')
+    # @Api.token_required(auth=True)
     def get(self):
         """
         Get Tags
